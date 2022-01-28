5f1d7ff06c14e884f055549a0e7e019a3aaef881
Eduardo <ealvarez@carminaticonsulting.com>
2022-01-28 17:45:05 -0600

    Got API working on react project

--- soccer/src/App.js
+++ soccer/src/App.js
@@ -2,11 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+var myHeaders = new Headers();
+myHeaders.append("X-Auth-Token", "013031e615294e3888556572314e2c2a");
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch("http://api.football-data.org/v2/competitions/2000/teams", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        
+        <p>{result}</p>
       </header>
     </div>
   );
